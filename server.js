6a7aed811bd4a2f62f5d72b74585b2aebc3e2dbd
Glitch (hot-front) <none>
2017-11-25 05:47:23 +0000

    🎃☂️ Updated with Glitch

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 // server.js
 // where your node app starts
 
-// init project
 var express = require('express');
-var chrono = require('chrono-node');
-
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
@@ -18,45 +11,70 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-//app.get("/stone", function (request, response) {
- // response.sendFile(__dirname + '/views/stone.html');
-//});
-
-//request syntax
-//$.post('/dreams?' + $.param({dream: dream})
-
 //could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response){
-  var stringIn = request.query.dream;
-  var toCheck = new Date(Date.parse(stringIn));
-  //convert to date
-  //convert date to ISO
-  //convert date to UNIX
-  //check if date.toString === request.query.dream.toString
-  if(toCheck.getTime() == stringIn){
-    response.send(JSON.parse( { "UNIX": toCheck.getTime(), "Natural Language": toCheck.toDateString() }));
+app.get("/:timeIn", function (request, response){
+  response.type('json');
+  
+  //implentation logic, first check if it's unix time
+  var timeIn = parseInt(request.params.timeIn.slice(0,15)); //first set of request digits build into a new integer
+  var toCheck = new Date(parseInt(timeIn));  
+  //tester response.send(jfyer(timeIn, toCheck.getTime()));
+  
+  //easy condition - compare Date method ms getter to the parsed timeIn
+  if(toCheck.getTime() == timeIn){ 
+    response.send(jfyer(toCheck.getTime(), toCheck.toDateString()));
   }
-  else if(chrono.parseDate(stringIn) !== null ){
-    response.send(JSON.parse( { "UNIX": toCheck.getTime(), "Natural Language": toCheck.toDateString() }));
+  
+  //hard condition - check if natural language
+  var toParse = request.params.timeIn; //Unprocessed String
+  //check if contains
+    //words
+      //month [full names or abbreviations] *no spell check
+      //day of week   [full names or abbreviations] *no spell check 
+    //numbers
+      //day date
+      //year
+      //numerical date
+      //time [hours, minutes, seconds, miliseconds]
+    //priority of values: Year, Month, Day
+    //defaults if absent
+      //current year
+      //current month
+      //current day?
+      //midnight
+  
+  //tokenize (whitespace) (trim commas)
+  //token by token [what is it?] > populate fields
+    //is it a word? .toLowerCase() is it [January, February, etc] [Jan, Feb, etc] [Monday, Tuesday, etc...] [Mon, Tue, Tues, etc...]
+      //populate Month and or Day
+    //contains digits?
+      //contains colon (:)? (probably a time) >
+        //how many digits on left side? (1 or 2) + ( < "24") = hour
+        //digits on right side? (2) = minute > digits right of minute? = seconds > digits right of seconds? = miliseconds
+      //contains a slash? (probably a Month, Day date or Year)
+        //how many digits to left of slash? (1 or 2?) + (less than 13) = month >
+        //how many digits to right of slash? (1 or 2?) + (less than 32) = day >
+        //digits to the right of? (2 or 4?) = year
+  //new date object
+  //populate date obj with values
+  
+  else if(new Boolean(true)/*timeIn == natural language*/){
+    response.send(jfyer("DateObj.getTime\(\)","DateObj.toString\(\)"));
   }
-  else{
-    response.send(JSON.parse( { "UNIX": null, "Natural Language": null }));
+  
+  //null on all counts
+  else{     
+    response.send(jfyer(null,null));
   }
-  //dreams.push(request.query.dream);
-  //  response.sendFile(__dirname + '/views/stone.html');
-  //response.sendStatus(200);  
 });
 
+function jfyer (unix, nlang){
+  var jOut = { "UNIX": unix, "Natural Language": nlang };
+  var stringOut = JSON.stringify(jOut);   
+  return stringOut;
+}
 
-// Simple in-memory store for now
-
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
+// listen for requests
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
