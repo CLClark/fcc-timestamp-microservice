379fb51f91d732e079d963b4a33f91972290c9bf
Glitch (hot-front) <none>
2017-11-26 03:21:13 +0000

    ⛑🌄 Updated with Glitch

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 var express = require('express');
+var chrono = require('chrono-node');
 var app = express();
 
 app.use(express.static('public'));
@@ -16,60 +17,53 @@ app.get("/:timeIn", function (request, response){
   response.type('json');
   
   //implentation logic, first check if it's unix time
-  var timeIn = parseInt(request.params.timeIn.slice(0,15)); //first set of request digits build into a new integer
-  var toCheck = new Date(parseInt(timeIn));  
-  //tester response.send(jfyer(timeIn, toCheck.getTime()));
+  var timeIn = (parseInt(request.params.timeIn)*1000); //unix convert-from
+  var toCheck = new Date(timeIn);  
+  var toParse = request.params.timeIn.toLowerCase(); //Unprocessed String
+  
+  var options = { 
+          //timeZone: 'UTC', 
+          //timeZoneName: 'short',
+          weekday: 'short',
+          month: 'long',
+          year: 'numeric',
+          day: 'numeric',
+          //hour: '2-digit',
+          //minute: '2-digit'          
+        };
   
   //easy condition - compare Date method ms getter to the parsed timeIn
   if(toCheck.getTime() == timeIn){ 
-    response.send(jfyer(toCheck.getTime(), toCheck.toDateString()));
+    response.send(jfyer((timeIn/1000),toCheck.toLocaleDateString('en-US',options))); //unix convert-to
   }
-  
-  //hard condition - check if natural language
-  var toParse = request.params.timeIn; //Unprocessed String
-  //check if contains
-    //words
-      //month [full names or abbreviations] *no spell check
-      //day of week   [full names or abbreviations] *no spell check 
-    //numbers
-      //day date
-      //year
-      //numerical date
-      //time [hours, minutes, seconds, miliseconds]
-    //priority of values: Year, Month, Day
-    //defaults if absent
-      //current year
-      //current month
-      //current day?
-      //midnight
-  
-  //tokenize (whitespace) (trim commas)
-  //token by token [what is it?] > populate fields
-    //is it a word? .toLowerCase() is it [January, February, etc] [Jan, Feb, etc] [Monday, Tuesday, etc...] [Mon, Tue, Tues, etc...]
-      //populate Month and or Day
-    //contains digits?
-      //contains colon (:)? (probably a time) >
-        //how many digits on left side? (1 or 2) + ( < "24") = hour
-        //digits on right side? (2) = minute > digits right of minute? = seconds > digits right of seconds? = miliseconds
-      //contains a slash? (probably a Month, Day date or Year)
-        //how many digits to left of slash? (1 or 2?) + (less than 13) = month >
-        //how many digits to right of slash? (1 or 2?) + (less than 32) = day >
-        //digits to the right of? (2 or 4?) = year
-  //new date object
-  //populate date obj with values
-  
-  else if(new Boolean(true)/*timeIn == natural language*/){
-    response.send(jfyer("DateObj.getTime\(\)","DateObj.toString\(\)"));
-  }
-  
-  //null on all counts
-  else{     
-    response.send(jfyer(null,null));
-  }
+  //natural language check
+  else{
+    //pass results of parseDate into callback
+    syncMaker(toParse, function cb(gottenBack){
+      //check if parseDate returned a good object
+      //weekday, year, month, day, hour, minute, second
+      if (gottenBack !== null ){        
+        var unixOut = (gottenBack.getTime()/1000); //unix convert-to
+        var natLOut = gottenBack.toLocaleDateString('en-US',options);
+        response.send(jfyer(unixOut,natLOut));
+      }      
+      else{
+        response.send(jfyer(null,null));
+      }
+    });        
+  }    
 });
 
-function jfyer (unix, nlang){
-  var jOut = { "UNIX": unix, "Natural Language": nlang };
+//sync the calls and response
+function syncMaker(whatToParse, next){
+  var dateObjOne = chrono.parseDate(whatToParse);    
+  //pass results of chrono-node into callback
+  next(dateObjOne);
+}
+
+//simplify the response calls
+function jfyer (unix, nlang){  
+  var jOut = { "unix": unix, "natural-language": nlang };
   var stringOut = JSON.stringify(jOut);   
   return stringOut;
 }
